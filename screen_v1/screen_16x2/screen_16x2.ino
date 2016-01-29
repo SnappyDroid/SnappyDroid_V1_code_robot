c14eadc3ee8f9dfc13b467c39f576e98bd314860
SinesioBittencourt <contato@sinesio.com.br>
2016-01-28 23:59:21 -0200

    up

---
+++ screen_v1/screen_16x2/screen_16x2.ino
@@ -0,0 +1,37 @@
+
+
+#include <FastIO.h>
+#include <I2CIO.h>
+#include <LCD.h>
+#include <LiquidCrystal.h>
+#include <LiquidCrystal_I2C.h>
+#include <LiquidCrystal_SR.h>
+#include <LiquidCrystal_SR2W.h>
+#include <LiquidCrystal_SR3W.h>
+
+// Programa : Display LCD 16x2 e modulo I2C
+// Autor : Arduino e Cia
+
+#include <Wire.h>
+#include <LiquidCrystal_I2C.h>
+
+// Inicializa o display no endereco 0x27
+LiquidCrystal_I2C lcd(0x27,2,1,0,4,5,6,7,3, POSITIVE);
+ 
+void setup()
+{
+ lcd.begin (16,2);
+}
+ 
+void loop()
+{
+  lcd.setBacklight(HIGH);
+  lcd.setCursor(0,0);
+  lcd.print("Arduino e Cia !!");
+  lcd.setCursor(0,1);
+  lcd.print("LCDmodulo I2C");
+  delay(1000);
+  lcd.setBacklight(LOW);
+  delay(1000);
+}
+
